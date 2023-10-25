ab58d3fe1b9dd74fd0ad24bb295793f606abff89
KakitapalliSunilKiran <k.sunilkiran98@gmail.com>
2023-10-25 09:44:25 +0530

    first commit

---
+++ src/app/component1/component1.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class Component1Component {
+
+}
